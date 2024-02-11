be3df3fc916cb8c96262b85280dfaed81ac0247c
SAPranto <sa.pranto16@gmail.com>
2024-02-11 19:52:59 +0600

    Intial Setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <section className="px-5 py-28">
+      <div className="container">
+        <h1 className="text-3xl font-bold">Heading</h1>
+      </div>
+    </section>
+  );
+}
