a30d089e0387c0eb75ec55eb43a0b2ce50f09854
SAPranto <sa.pranto16@gmail.com>
2024-02-17 16:46:18 +0600

    Navbar Function Completed

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -17,22 +17,18 @@ const navbar = () => {
         <div
           className={`text-center bg-red-500 absolute top-0 right-0 px-24 py-14 h-full ${
             isNavOpen ? "block" : "hidden"
-          }`}
+          } lg:block lg:relative lg:bg-transparent lg:py-0 lg:px-0`}
         >
-          <ul className="flex flex-col gap-10">
+          <ul className="flex flex-col items-center gap-10 lg:flex-row">
             <li>Home</li>
             <li>About</li>
             <li>Contact</li>
             <li>Career</li>
             <li className="primary-btn">Button</li>
           </ul>
-          <div className="absolute top-4 right-4" onClick={onNavClick}>
-            <GiCancel className="cursor-pointer"></GiCancel>
-          </div>
+            <GiCancel className="absolute top-4 right-4 cursor-pointer lg:hidden" onClick={onNavClick}></GiCancel>
         </div>
-        <div onClick={onNavClick}>
-          <GiHamburgerMenu className="cursor-pointer"></GiHamburgerMenu>
-        </div>
+          <GiHamburgerMenu className="cursor-pointer lg:hidden" onClick={onNavClick}></GiHamburgerMenu>
       </div>
     </nav>
   );
